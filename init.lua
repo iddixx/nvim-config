777e1e513348ad2680af525c7730039278bd61c4
iddixx <125816906+ONMUQU@users.noreply.github.com>
2025-03-06 19:44:56 +0200

    added packer bootstrap

--- init.lua
+++ init.lua
@@ -198,6 +198,20 @@ end
 
 colemak_setup()
 -- packer
+
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
 vim.cmd [[packadd packer.nvim]]
 
 require('packer').startup(function(use)
@@ -328,6 +342,9 @@ require('packer').startup(function(use)
     use 'sphamba/smear-cursor.nvim' -- smooth cursor
     use 'karb94/neoscroll.nvim'     -- smooth scroll
     use 'markonm/traces.vim'        -- highlights patterns in command mode
+    if packer_bootstrap then
+        require('packer').sync()
+    end
 end)
 -- gui settings
 
