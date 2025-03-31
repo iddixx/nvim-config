38fb96f089612993bc72af7c220390f62a6f6dd0
iddixx <125816906+ONMUQU@users.noreply.github.com>
2025-03-31 23:26:51 +0300

    configured oil.nvim little bit

--- init.lua
+++ init.lua
@@ -14,7 +14,6 @@ Getting servername:
 -- based on https://github.com/linduxed/colemak.nvim
 
 local colemak_mappings = {
-
     -- Up/down/left/right
     { modes = { "n", "v", "o", "x" }, lhs = "n",          rhs = "h",      desc = "Left (h)" },
     { modes = { "n", "v", "o", "x" }, lhs = "u",          rhs = "gk",     desc = "Visual Up (gk)" },
@@ -299,7 +298,30 @@ require('pckr').add({
     {
         "stevearc/oil.nvim",
         config = function()
-          require("oil").setup()
+                require("oil").setup({
+                keymaps = {
+                   ["g?"] = { "actions.show_help", mode = "n" },
+                   ["<CR>"] = "actions.select",
+                   ["<C-s>"] = { "actions.select", opts = { vertical = true } },
+                   ["<C-h>"] = { "actions.select", opts = { horizontal = true } },
+                   ["<C-t>"] = { "actions.select", opts = { tab = true } },
+                   ["<C-p>"] = "actions.preview",
+                   ["<C-c>"] = { "actions.close", mode = "n" },
+                   ["<C-l>"] = "actions.refresh",
+                   ["-"] = { "actions.parent", mode = "n" },
+                   ["_"] = { "actions.open_cwd", mode = "n" },
+                   ["`"] = { "actions.cd", mode = "n" },
+                   ["~"] = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
+                   ["gs"] = { "actions.change_sort", mode = "n" },
+                   ["gx"] = "actions.open_external",
+                   ["g."] = { "actions.toggle_hidden", mode = "n" },
+                   ["g\\"] = { "actions.toggle_trash", mode = "n" },
+                },
+                use_default_keymaps = true,
+                view_options = {
+                    show_hidden = true,
+                }
+            })
         end,
     },
 
